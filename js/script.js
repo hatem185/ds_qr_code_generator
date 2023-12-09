cec32be8726e727f2ddedbd09c785e91efe422c7
hatem <hatememhemed.2000@gmail.com>
2023-12-09 15:32:11 +0200

    Add show all page of qr code button

--- js/script.js
+++ js/script.js
@@ -9,14 +9,33 @@ const qtyCode = document.getElementById("qty-code");
 const clientText = document.getElementById("client-qr-code");
 const typeQrCodeText = document.getElementById("type-qr-code");
 const loadingIndicator = document.getElementById("loading-indicator");
+const showAllPagesBtn = document.getElementById("show-all-page");
 let lastItemPage = 0;
 let qrCodeList = [];
 let qrPage = [];
 let isGenerated = false;
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+showAllPagesBtn.addEventListener("click", async () => {
+  const isShowingAll =
+    showAllPagesBtn.textContent.trim().toLowerCase() === "show all";
+  showAllPagesBtn.textContent = isShowingAll ? "Hide all" : "Show all";
+  qrPageSwitcher.classList.toggle("hidden", isShowingAll);
+  if (isShowingAll) {
+    removeAllChildren(qrCodeContainer);
+    qrCodeContainer.append(...qrCodeList);
+  } else {
+    removeAllChildren(qrCodeContainer);
+    qrCodeContainer.append(...qrPage);
+  }
+});
 nextPage.addEventListener("click", async (e) => {
   e.preventDefault();
   if (qrCodeList.length <= lastItemPage) return;
-  qrPage.forEach((child) => qrCodeContainer?.removeChild(child));
+  removeAllChildren(qrCodeContainer);
   qrPage = qrCodeList.slice(lastItemPage, lastItemPage + 6);
   qrCodeContainer.append(...qrPage);
   lastItemPage += qrPage.length;
@@ -25,7 +44,7 @@ prevPage.addEventListener("click", async (e) => {
   e.preventDefault();
   if (lastItemPage <= 6) return;
   lastItemPage -= qrPage.length;
-  qrPage.forEach((child) => qrCodeContainer?.removeChild(child));
+  removeAllChildren(qrCodeContainer);
   qrPage = qrCodeList.slice(lastItemPage - 6, lastItemPage);
   qrCodeContainer.append(...qrPage);
 });
@@ -33,7 +52,7 @@ generateBtn.addEventListener("click", async () => {
   if (isGenerated) return;
   const qty = qtyCode.value ?? 0;
   if (qty <= 0) return;
-  qrPage.forEach((child) => qrCodeContainer?.removeChild(child));
+  removeAllChildren(qrCodeContainer);
   qrCodeList = [];
   qrPage = [];
   lastItemPage = 0;
@@ -80,9 +99,11 @@ generateBtn.addEventListener("click", async () => {
     qrCodeContainer.append(...qrPage);
     loadingIndicator.classList.add("hidden");
     if (qrCodeList.length > 6) {
+      showAllPagesBtn.classList.remove("hidden");
       qrPageSwitcher.classList.remove("hidden");
     } else {
       qrPageSwitcher.classList.add("hidden");
+      showAllPagesBtn.classList.add("hidden");
     }
     isGenerated = false;
   }, 1000);
